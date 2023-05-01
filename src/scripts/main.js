4f3fab07b44ada8d13cd873dc4eaba397a593473
Paola Freitas <pahnetone@Paolas-MacBook-Air.local>
2023-05-01 13:55:04 -0300

    criando countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("hello");
+AOS.init();
+
+const eventDate = new Date("Dec 12, 2023 19:00:00");
+const timeStampEvent = eventDate.getTime();
+
+const timeCount = setInterval(function () {
+  const now = new Date();
+  const timeStampNow = now.getTime();
+
+  const timeToEvent = timeStampEvent - timeStampNow;
+  const minutesInMs = 1000 * 60;
+  const hoursInMs = minutesInMs * 60;
+  const daysInMs = hoursInMs * 24;
+
+  const daysToEvent = Math.floor(timeToEvent / daysInMs);
+  const hoursToEvent = Math.floor((timeToEvent % daysInMs) / hoursInMs);
+  const minutesToEvent = Math.floor((timeToEvent % hoursInMs) / minutesInMs);
+  const secondsToEvent = Math.floor((timeToEvent % minutesInMs) / 1000);
+
+  document.getElementById(
+    "counter"
+  ).innerHTML = `${daysToEvent} d ${hoursToEvent}h ${minutesToEvent}m ${secondsToEvent}s`;
+
+  if (timeToEvent < 0) {
+    clearInterval(timeCount);
+    document.getElementById("counter").innerHTML = `Evento expirado`;
+  }
+}, 1000);
